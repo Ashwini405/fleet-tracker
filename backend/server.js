cae9a61cca16a41488c8b4741a8acbf8594698ff
Ashwini <panianu8@gmail.com>
2026-04-29 15:05:49 +0530

    fix: use *splat wildcard for Express 5 catch-all route

--- backend/server.js
+++ backend/server.js
@@ -117,7 +117,7 @@ app.post('/logs', async (req, res) => {
 });
 
 // Serve React app for all other routes
-app.get('*', (req, res) => {
+app.get('*splat', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
